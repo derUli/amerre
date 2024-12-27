a6dc8b8a3e4d98ae6c93c9c421d6afbb800d1929
Uli <ulidots@ulidots.de>
2024-12-27 13:09:58 +0100

    Style

--- src/app/utils/callbacks.py
+++ src/app/utils/callbacks.py
@@ -1,3 +1,10 @@
+""" Callback methods """
+
+
 class Callbacks:
+    """ Callback methods """
+
     def __init__(self, on_level_completed):
+        """ Constructor """
+
         self.on_level_completed = on_level_completed
