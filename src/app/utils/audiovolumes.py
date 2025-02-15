e56cd641ac7ae292c1f6fee38068f9ad16c95ba5
Uli <ulidots@ulidots.de>
2025-02-15 21:43:04 +0100

    Speech

--- src/app/utils/audiovolumes.py
+++ src/app/utils/audiovolumes.py
@@ -20,39 +20,55 @@ class AudioVolumes:
 
     @property
     def volume_master(self) -> int:
+        """ Volume master """
+
         return self._volume_master
 
     @volume_master.setter
     def volume_master(self, value: int):
+        """ Volume master """
+
         self._volume_master = value
 
     @property
     def volume_sound(self) -> int:
+        """ Volume sound """
+
         return self._volume_sound
 
     @volume_sound.setter
     def volume_sound(self, value: int):
+        """ Volume sound """
+
         self._volume_sound = value
 
     @property
     def volume_speech(self) -> int:
+        """ Volume speech """
+
         return self._volume_speech
 
     @volume_speech.setter
     def volume_speech(self, value: int):
+        """ Volume speech """
+
         self._volume_speech = value
 
     @property
     def volume_music(self) -> int:
+        """ Volume music """
+
         return self._volume_music
 
     @volume_music.setter
     def volume_music(self, value: int):
+        """ Volume music """
+
         self._volume_music = value
 
     @property
     def volume_master_normalized(self) -> float:
-        """ Master volume converted to float """
+        """ Normalized master volume as float """
 
         if self._volume_master <= 0:
             return 0.0
@@ -61,7 +77,7 @@ class AudioVolumes:
 
     @property
     def volume_music_normalized(self) -> float:
-        """ Music volume converted to float """
+        """ Normalized music volume as float """
 
         if self._volume_music <= 0:
             return 0.0
@@ -70,7 +86,7 @@ class AudioVolumes:
 
     @property
     def volume_sound_normalized(self) -> float:
-        """ Sound volume converted to float """
+        """ Normalized sound volume as float """
 
         if self._volume_sound <= 0:
             return 0.0
@@ -79,7 +95,7 @@ class AudioVolumes:
 
     @property
     def volume_speech_normalized(self) -> float:
-        """ Speech volume converted to float """
+        """ Normalized speech volume as float """
 
         if self._volume_speech <= 0:
             return 0.0
