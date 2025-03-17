2bd13ac3646914fe645f3bac06cb580a4f153b1b
Uli <ulidots@ulidots.de>
2025-03-17 16:23:06 +0100

    Log audio driver

--- src/app/helpers/localization.py
+++ src/app/helpers/localization.py
@@ -4,9 +4,10 @@ from app.constants.gameinfo import LOCALES_ALL, LOCALE_FALLBACK
 
 
 def default_language():
+    """ Detect default langauge """
+
     ls = list(locale.getlocale())
-    ls = map(lambda s: s.split('_'), ls)
-    ls = map(lambda s: s[0].lower(), ls)
+    ls = map(lambda s: s[:2].lower(), ls)
     ls = list(filter(lambda s: s in LOCALES_ALL, ls))
 
     if not any(ls):
